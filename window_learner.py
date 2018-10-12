aa1b8ed03e92ef9714d17ca7aeef0b734146ef4d
Nacho en el iimas <iaf@ccg.unam.mx>
2018-10-11 20:36:03 -0500

    listo el traga XMLs

--- window_learner.py
+++ window_learner.py
@@ -2,6 +2,9 @@ import sh
 import re
 from string import punctuation
 
+
+from pdb import set_trace as st
+
 punctuation = re.sub('[/|<|>]+', '', punctuation)
 
 def build_search_tags(tag_file, return_regexs=True):
@@ -22,18 +25,30 @@ def build_search_tags(tag_file, return_regexs=True):
 
 
 def build_window_dataset(lines, tagsregexs, winsize=5):
+
     samples = []
     for line in lines:
-        words = re.sub('['+punctuation+']', '', line).split()
+        clean = re.sub('['+punctuation+']', '', line)
+        clean = re.sub('><', '> <', clean)
+        old_list = [i for i in re.findall(r">([^<>]*)</", clean) if i != ' ']
+        new_list = [re.sub(' ', '_', item) for item in old_list]
+        words = clean
+        for old, new in zip(old_list, new_list):
+            words = re.sub(old, new, words)
+
+        words = words.split()[1:]
         for idx, word in enumerate(words):
             if word.startswith("<") and ("</" in word):
                 tag = re.search("</(.*)>", word).group(1)
                 start = idx - min([idx, winsize])
-                end = idx + min([idx - len(words) - 1, winsize])
-                sample = words[start:idx] + words[idx + 1:end]
+                end = idx + min([idx + len(words) - 1, winsize]) + 1
+                sample = ' '.join(words[start:idx] + words[idx + 1:end])
+                sample = re.sub('</?(.*)>', '', sample)
+                #sample = re.sub('<(.*)>', '', sample)
+                if sample != '':
+                     samples.append({"text": sample, "label": tag})
 
-        samples.append(sample)
-
+    return samples
 
 inxml = "/home/iarroyof/Dropbox/ES_Carlos_Ignacio/xhGCs/paquete-Nacho/ejemplos-etiquetado-xml/GSE54899_family.xml"
 tag_dict_f = "/home/iarroyof/Dropbox/ES_Carlos_Ignacio/xhGCs/paquete-Nacho/ejemplos-etiquetado-xml/esquema-gcs.xsd"
@@ -48,3 +63,12 @@ tag_regexps = build_search_tags(tag_dict_f)
 lines = sh.sed("-n", sample_regex, inxml)
 
 data = build_window_dataset(lines=lines, tagsregexs=tag_regexps, winsize=5)
+
+samples = []
+labels = []
+for d in data:
+    samples.append(d['text'])
+    labels.append(d['label'])
+
+print(samples)
+print(labels)
